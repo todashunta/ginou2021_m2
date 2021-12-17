89bb8b217d74886ae61335fe1eff06b18c25b4c1
todashunta <todashuntastem@gmail.com>
2021-12-18 00:06:55 +0900

    api

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -3,8 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\ClassShop;
 
 class ApiController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = ClassShop::query();
+        // dd(isset($request->gradeNumber));
+        if (!empty($request->gradeNumber)) {
+            $query->where('grade', $request->gradeNumber);
+        }
+        if (!empty($request->classNumber)) {
+            $query->where('class', $request->classNumber);
+        }
+        if (!empty($request->program)) {
+            $query->where('program', 'like', '%' . $request->program . '%');
+        }
+        if (!empty($request->food_flag)) {
+            $query->where('shop_flag', $request->food_flag);
+        }
+        $shop = $query->get();
+        return response()->json($shop);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+use App\Http\Controllers\ApiController;
+
+
+Route::post('/', [ApiController::class, 'index']);
